db0967c9aab00a48449e3a1c203f3480af9bd3a0
Osamah <osamah.islam@outlook.com>
2025-11-06 08:26:39 +0200

    login done

--- src/features/auth/pages/SignUpPage.tsx
+++ src/features/auth/pages/SignUpPage.tsx
@@ -64,18 +64,16 @@ export const SignUpPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-neutral-50 flex flex-col p-4">
-      {/* Mojeeb Brand Logo - Top of Page */}
-      <div className="pt-6 pb-8">
-        <img
-          src="/mojeeb-logo.png"
-          alt="Mojeeb"
-          className="h-6 mx-auto"
-        />
-      </div>
-
-      <div className="flex-1 flex items-center justify-center">
-        <div className="w-full max-w-md">
+    <div className="min-h-screen bg-neutral-50 flex items-center justify-center p-4">
+      <div className="w-full max-w-md">
+        {/* Mojeeb Brand Logo - Above Card */}
+        <div className="text-center mb-6">
+          <img
+            src="/mojeeb-logo.png"
+            alt="Mojeeb"
+            className="h-10 mx-auto"
+          />
+        </div>
 
         {/* Minimal Card Container */}
         <div className="bg-white rounded-xl border border-neutral-200 p-8">
@@ -155,7 +153,6 @@ export const SignUpPage = () => {
               </Link>
             </p>
           </div>
-        </div>
         </div>
       </div>
     </div>
